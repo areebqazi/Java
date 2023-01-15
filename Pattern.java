d7f4dd6eb88c2980cdf4c0ca56f30a3bb22ca72c
areebqazi <66074608+areebqazi@users.noreply.github.com>
2023-01-16 02:28:04 +0530

    hi

--- Pattern.java
+++ Pattern.java
@@ -1,20 +1,18 @@
-import java.util.*;
+ import java.util.*;
 public class Pattern {
+    
     public static void main(String args[]){
         Scanner s=new Scanner(System.in);
         int no=s.nextInt();
-        int a[][]=new int[2*(no-1)+1][2*(no-1)+1] ;
+        int a[][]=new int[2*no-1][2*no-1] ;
         int m=0;int n=a.length-1;
         int nn=no;
         for(int k=1;k<=no;k++){
                for(int i=m;i<a.length-m;i++){
                 for(int j=m;j<a.length-m;j++){
-                    if(i==m||i==n)
-                    a[i][j]=nn;
-                    if(j==m || j==n)
+                    if(i==m||i==n||j==m || j==n)
                     a[i][j]=nn;
                 }
-                
                }
                m++;
                n--;
